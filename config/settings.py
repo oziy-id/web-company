2b7397b0fb062396832c17c1e0cc1299a3cd928b
Ahmad Fauzi <oziy-id@users.noreply.huggingface.co>
2026-01-17 07:47:16 +0000

    Update config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -123,18 +123,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # KONFIGURASI EMAIL (VERSI AMAN)
 # ==========================================
 
+# settings.py (Bagian Paling Bawah)
+
+# ==========================================
+# KONFIGURASI EMAIL (VERSI AMAN & NGEBUT)
+# ==========================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 
-# Baca dari environment (rahasia)
+# Gunakan Port 465 dengan SSL (Lebih stabil untuk Hugging Face)
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+# Kode Panggilan (JANGAN TULIS EMAIL/PASS ASLI DISINI!)
+# Biarkan kode ini mengambil data dari "Secrets" Hugging Face
+import os # Pastikan sudah ada import os di paling atas file, atau tulis lagi disini gpp
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-# ... kode paling bawah settings.py ...
-
-# Tambahkan daftar domain yang dipercaya agar tidak kena blokir CSRF
-CSRF_TRUSTED_ORIGINS = [
-    'https://oziy-id-dongcai-garment.hf.space',
-]
-
